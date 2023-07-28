d7920ce145ef7591d1b416cf051628d4941c115b
Vinod Kumar <48450161+VinodJaka@users.noreply.github.com>
2023-07-29 01:26:15 +0530

    initial commit

---
+++ Routes/product.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const express= require('express');
+const product= express.Router();
+const mysql= require('mysql');
+
+const pool= mysql.createPool({
+    host:"localhost",
+    user:"root",
+   database:"estore1",
+    password:process.env.DB_PASSWORD,
+    port:3306,
+    multipleStatments: true
+})
+
+
+
+product.get("/productCategories",(req,res)=>{
+  let categorydata;
+  
+
+      pool.query("select * from categories",(err,categories)=>{
+        if(err){
+          categorydata=err;
+          res.status(500).send(categorydata);
+        }else{  categorydata=categories;
+          res.status(200).send(categorydata); }
+        
+      })
+    
+  })
+
+  product.get("/getProducts",(req,res)=>{
+    let productData;
+
+    pool.query("select * from products",(err,rows)=>{
+      if(err){
+        productData=err;
+        res.status(500).send(productData);
+      }else{ productData=rows ;
+        res.status(200).send(productData); }
+      
+    })
+
+  })
+
+  module.exports = product;
